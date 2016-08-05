4d09afd3e49022983fab3ac29d67b348309d8775
warren fung <wfung@e1z1r3p21.42.us.org>
2016-08-04 17:57:16 -0700

    lastparam

---
+++ aff_last_paramls/aff_last_param.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   aff_last_param.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wfung <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/08/04 17:27:51 by wfung             #+#    #+#             */
+/*   Updated: 2016/08/04 17:51:59 by wfung            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	aff_last(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int		main(int ac, char **av)
+{
+	if (ac < 2)
+		ft_putchar('\n');
+	else
+	{
+		aff_last((av[ac - 1]));
+		ft_putchar('\n');
+	}
+	return (0);
+}
